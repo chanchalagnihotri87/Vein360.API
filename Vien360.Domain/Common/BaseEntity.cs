2b200828c1a063220b8a7fdf3e462ebd678d6618
Chanchal <chanchalagnihotri1987@gmail.com>
2025-04-29 18:53:06 +0530

    Fixed create date issue.

--- Vien360.Domain/Common/BaseEntity.cs
+++ Vien360.Domain/Common/BaseEntity.cs
@@ -9,7 +9,7 @@ namespace Vien360.Domain.Common
     public class BaseEntity
     {
         public int Id { get; set; }
-        public DateTimeOffset CreatedDate { get; set; } = new DateTime(2025, 4, 16);
+        public DateTimeOffset CreatedDate { get; set; } = DateTime.Now;
         public DateTimeOffset? UpdatedDate { get; set; } = null;
         public DateTimeOffset? DeletedDate { get; set; } = null;
         public bool IsDeleted { get; set; } = false;
