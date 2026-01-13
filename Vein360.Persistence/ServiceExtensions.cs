94b948c8188f1a0daad028fba11fffd0db3f31a7
Chanchal <chanchalagnihotri1987@gmail.com>
2026-01-13 16:58:28 +0530

    Fixed SQL Query Logging issue.

--- Vein360.Persistence/ServiceExtensions.cs
+++ Vein360.Persistence/ServiceExtensions.cs
@@ -44,7 +44,12 @@ namespace Vein360.Persistence
         public static void ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
         {
 
-            services.AddDbContextPool<Vein360Context>(options => options.UseSqlServer(configuration.GetConnectionString("Vein360Context")));
+            services.AddDbContextPool<Vein360Context>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("Vein360Context"));
+                options.EnableSensitiveDataLogging(false);
+            }
+            );
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IDonationRepository, DonationRepository>();
